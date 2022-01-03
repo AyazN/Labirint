d507dd167f21769a6cb97c9da1be9891babd9b0d
DamirMukhametzyanov <92740583+DamirMukhametzyanov@users.noreply.github.com>
2022-01-03 13:33:08 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,20 @@
-RES = WIDTH, HEIGHT = 1200, 800
+RES = WIDTH, HEIGHT = 1250, 850
+RES_MENU = (800, 600)
 TILE = 50
 FPS = 60
 COLS, ROWS = WIDTH // TILE, HEIGHT // TILE
-
+GRAVITY = 1
+CORDS = [(WIDTH // 2, HEIGHT // 2), (WIDTH // 3, HEIGHT // 3), (WIDTH // 3 * 2, HEIGHT // 3)]
+NAME_DB = 'SQL/labirint_db.db'
+LOG = 'log'
+MAPS = 'maps'
+ACCOUNTS = 'accounts'
+ID = 'id'
+ACCOUNT = 'account'
+PLAYER_X = 'player_x'
+PLAYER_Y = 'player_y'
+ENEMY_X = 'enemy_x'
+ENEMY_Y = 'enemy_y'
+MAP = 'map'
+LOGIN = 'login'
+PASSWORD = 'password'
