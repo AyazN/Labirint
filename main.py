2de3f6597287fb9032853371dadeaefde6090e3f
DamirMukhametzyanov <92740583+DamirMukhametzyanov@users.noreply.github.com>
2022-01-05 23:40:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,37 +7,37 @@ from mini_game import main_noughts_crosses
 from final import *
 from player import *
 
-num_exit = 0
+num_exit = ZERO
 
 
 def save_game(enemy, player, img_kill, user_id):
     if enemy.collision_check(player) and not img_kill:
-        add_new_save(user_id, player.get_pos()[0], player.get_pos()[1], enemy.start_pos_rt()[0],
-                     enemy.start_pos_rt()[1],
+        add_new_save(user_id, player.get_pos()[ZERO], player.get_pos()[ONE], enemy.start_pos_rt()[ZERO],
+                     enemy.start_pos_rt()[ONE],
                      select_map_id(open_map()))
     else:
-        add_new_save(user_id, player.get_pos()[0], player.get_pos()[1], enemy.get_pos()[0][0],
-                     enemy.get_pos()[0][1],
+        add_new_save(user_id, player.get_pos()[ZERO], player.get_pos()[ONE], enemy.get_pos()[ZERO][ZERO],
+                     enemy.get_pos()[ZERO][ONE],
                      select_map_id(open_map()))
 
 
 def redraw_new_window(screen, dead, win, enemy, player, img_kill, user_id):
     global num_exit
-    my_font = pygame.font.SysFont('arial', 130)
-    text_surface = my_font.render('YOU WIN', False, (255, 204, 0))
-    text_surface_died = my_font.render('YOU DIED', False, (255, 0, 0))
+    my_font = pygame.font.SysFont('arial', FONT_RES)
+    text_surface = my_font.render('YOU WIN', False, GOLD)
+    text_surface_died = my_font.render('YOU DIED', False, RED_COLOR)
     if dead:
         save_game(enemy, player, img_kill, user_id)
         screen.fill('Black')
-        screen.blit(text_surface_died, (WIDTH // 4 + 50, HEIGHT // 3))
-        num_exit += 1
+        screen.blit(text_surface_died, (WIDTH // OME_OF_FOUR + TILE, HEIGHT // ONE_OF_THREE))
+        num_exit += ONE_ITERATION
     elif win:
         screen.fill('Black')
-        screen.blit(text_surface, (WIDTH // 3, HEIGHT // 3))
-        create_particles((random.randint(1, WIDTH), random.randint(1, HEIGHT)))
+        screen.blit(text_surface, (WIDTH // ONE_OF_THREE, HEIGHT // ONE_OF_THREE))
+        create_particles((random.randint(ONE_COORD, WIDTH), random.randint(ONE_COORD, HEIGHT)))
         all_sprites_new.draw(screen)
         all_sprites_new.update()
-        num_exit += 1
+        num_exit += ONE_ITERATION
     else:
         all_sprites.draw(screen)
         all_sprites.update()
@@ -49,7 +49,8 @@ def check_all_nulls():
     return False
 
 
-def main_play(running, user_id, player_pos=(1, 1), enemy_pos=(23, 1), play_nulls=True, total=0, num=0, dead=False,
+def main_play(running, user_id, player_pos=SPAWN_PLAYER, enemy_pos=SPAWN_ENEMY, play_nulls=True, total=ZERO, num=ZERO,
+              dead=False,
               img_kill=False, img_fire=False, win=False, killing=False):
     global num_exit
     pygame.init()
@@ -57,7 +58,7 @@ def main_play(running, user_id, player_pos=(1, 1), enemy_pos=(23, 1), play_nulls
     player, enemy, level_x, level_y = generate_level(load_level('map'))
     player.pos_x, player.pos_y = player_pos
     enemy.pos_x, enemy.pos_y = enemy_pos
-    player.player_move(0, 0)
+    player.player_move(ZERO, ZERO)
     clock = pygame.time.Clock()
     if img_kill:
         enemy.image = load_image('kill.png')
@@ -69,48 +70,46 @@ def main_play(running, user_id, player_pos=(1, 1), enemy_pos=(23, 1), play_nulls
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    player.player_move(-1, 0)
+                    player.player_move(MOVE_LEFT)
                 if event.key == pygame.K_RIGHT:
-                    player.player_move(1, 0)
+                    player.player_move(MOVE_RIGHT)
                 if event.key == pygame.K_UP:
-                    player.player_move(0, -1)
+                    player.player_move(MOVE_UP)
                 if event.key == pygame.K_DOWN:
-                    player.player_move(0, 1)
+                    player.player_move(MOVE_DOWN)
                 if event.key == pygame.K_SPACE and player.get_pos() in enemy.get_pos():
                     enemy.image = load_image('kill.png')
                     img_kill = True
                     killing = True
         screen.fill('Grey')
-        if num_exit % 100 == 0 and num_exit > 100:
+        if num_exit % BIG_ITERATION == ZERO and num_exit > BIG_ITERATION:
             pygame.display.flip()
             running = False
             continue
-        if bool(nulls) and player.get_pos() == nulls[0] and play_nulls:
+        if bool(nulls) and player.get_pos() == nulls[ZERO] and play_nulls:
             dead = check_all_nulls()
-            main_play(running, user_id, player.get_pos(), enemy.get_pos()[0], play_nulls=False,
+            main_play(running, user_id, player.get_pos(), enemy.get_pos()[ZERO], play_nulls=False,
                       img_kill=img_kill, img_fire=img_fire, killing=killing, total=total, num=num)
             running = False
             continue
         save_game(enemy, player, img_kill, user_id)
         if ((enemy.collision_check(player) and not img_kill) or
-           (enemy.collision_check_with_fire(player) and img_fire)) and not dead:
+            (enemy.collision_check_with_fire(player) and img_fire)) and not dead:
             dead = True
             save_game(enemy, player, img_kill, user_id)
-        if player.get_pos() == castle[0]:
+        if player.get_pos() == castle[ZERO]:
             win = True
-        if total % 25 == 0 and total > 25 and not win and not dead:
+        if total % SMALL_ITERATION == ZERO and total > SMALL_ITERATION and not win and not dead:
             enemy.enemy_move(player, killing)
-        if total % 100 == 0 and total > 100 and not win and not dead:
-            tile_images['frame'] = load_image('fire.png') if num % 2 == 0 else load_image('frame.png')
+        if total % BIG_ITERATION == ZERO and total > BIG_ITERATION and not win and not dead:
+            tile_images['frame'] = load_image('fire.png') if num % NUM_FIRE_FACE == ZERO else load_image('frame.png')
             img_fire = True if not img_fire else False
-            [Tile('frame', fires[i][0], fires[i][1]) for i in range(len(fires))]
-            num += 1
+            [Tile('frame', fires[i][ZERO], fires[i][ONE]) for i in range(len(fires))]
+            num += ONE_ITERATION
         redraw_new_window(screen, dead, win, enemy, player, img_kill, user_id)
-        total += 1
+        total += ONE_ITERATION
         pygame.display.flip()
         clock.tick(FPS)
     save_game(enemy, player, img_kill, user_id)
     pygame.quit()
-
-
 
